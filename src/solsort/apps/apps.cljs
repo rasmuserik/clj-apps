793fc2e77e6d7bc002d876d54ec483ae2b37e471
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-01-25 11:22:33 +0100

    indent via emacs

--- src/solsort/apps/apps.cljs
+++ src/solsort/apps/apps.cljs
@@ -1,15 +1,15 @@
 (ns solsort.apps.apps
   (:require-macros  [cljs.core.async.macros :refer  [go go-loop alt!]])
   (:require
-    [solsort.util :refer  [<ajax <seq<! js-seq normalize-css load-style! put!close! parse-json-or-nil]]
-    [reagent.core :as reagent :refer  []]
-    [clojure.string :as string :refer [replace split blank?]]
-    [cljs.core.async :refer  [>! <! chan put! take! timeout close! pipe]]))
+   [solsort.util :refer  [<ajax <seq<! js-seq normalize-css load-style! put!close! parse-json-or-nil]]
+   [reagent.core :as reagent :refer  []]
+   [clojure.string :as string :refer [replace split blank?]]
+   [cljs.core.async :refer  [>! <! chan put! take! timeout close! pipe]]))
 ;; # Utility functions
 (def months
   (string/split
-    " January February March April May June July August September October November December"
-    #" "))
+   " January February March April May June July August September October November December"
+   #" "))
 (defn log  [& args]
   (js/console.log  (clj->js args))
   (first args))
@@ -43,9 +43,9 @@
 ;; # Application state
 (defonce db 
   (reagent/atom 
-    {:entries []
-     :posts []
-     }))
+   {:entries []
+    :posts []
+    }))
 
 ;; # db-changes
 (defn update-route []
@@ -54,8 +54,8 @@
 (defn update-entry [o]
   (swap! db assoc :entries 
          (map
-           (fn [entry] (if (= (:id entry) (:id o)) o entry))
-           (:entries @db))))
+          (fn [entry] (if (= (:id entry) (:id o)) o entry))
+          (:entries @db))))
 ;; # Events
 (defn set-route [& route]
   (fn [] (aset js/location "hash" (string/join ":" route))))
@@ -65,76 +65,76 @@
 (defonce initialisation
   (do
     (.initializeTouchEvents js/React true)
-;; ## Blog posts
-(defn <simple-xhr [url]
-  (let [c (chan)
-        xhr (js/XMLHttpRequest.)]
-    (aset xhr "onreadystatechange" 
-          (fn [a]
-            (when (= 4 (aget xhr "readyState"))
-              (put!close! c (aget xhr "responseText")))))
-    (.open xhr "GET" url)
-    (.send xhr)
-    c))
-(go
-  (swap! db assoc :posts [])
-  (loop [i 1]
-    (let 
-      [posts (<! (<simple-xhr (str "https://blog.solsort.com/wp-json/wp/v2/posts?page=" i)))
-       posts (parse-json-or-nil posts)]
-      (when (< 0 (.-length posts))
-        (swap! db assoc :posts (into (:posts @db) posts))
-        (recur (inc i))))))
-;; ## App listing
-(defn conf-id [o]
-  (if (= :preference (:tag o))
-    (:name (:attrs o))
-    (:tag o)))
+    ;; ## Blog posts
+    (defn <simple-xhr [url]
+      (let [c (chan)
+            xhr (js/XMLHttpRequest.)]
+        (aset xhr "onreadystatechange" 
+              (fn [a]
+                (when (= 4 (aget xhr "readyState"))
+                  (put!close! c (aget xhr "responseText")))))
+        (.open xhr "GET" url)
+        (.send xhr)
+        c))
+    (go
+      (swap! db assoc :posts [])
+      (loop [i 1]
+        (let 
+            [posts (<! (<simple-xhr (str "https://blog.solsort.com/wp-json/wp/v2/posts?page=" i)))
+             posts (parse-json-or-nil posts)]
+          (when (< 0 (.-length posts))
+            (swap! db assoc :posts (into (:posts @db) posts))
+            (recur (inc i))))))
+    ;; ## App listing
+    (defn conf-id [o]
+      (if (= :preference (:tag o))
+        (:name (:attrs o))
+        (:tag o)))
 
-(go
-  (let 
-    [entries(<! (<ajax "assets/repos.lst" :result "text"))
-     entries (split entries "\n")
-     entries (map 
-               (fn [repo]
-                 (let [id (replace repo #".*/" "")]
-                   {:id id
-                    :title id
-                    :github repo}))
-               entries)]
-    (swap! db assoc :entries entries)
-    (doall (for [entry entries]
-             (go
-               (let [id (:id entry)
-                     config-xml (<! (<ajax (str "/" id "/config.xml") :result "text"))
-                     config-dom (.parseFromString  (js/DOMParser.) config-xml "application/xml")
-                     config (dom->clj config-dom)
-                     widget (first (:children config))   
-                     config-elems (into {} (map (fn [e] [(conf-id e) e]) (:children widget)))
-                     ]
+    (go
+      (let 
+          [entries(<! (<ajax "assets/repos.lst" :result "text"))
+           entries (split entries "\n")
+           entries (map 
+                    (fn [repo]
+                      (let [id (replace repo #".*/" "")]
+                        {:id id
+                         :title id
+                         :github repo}))
+                    entries)]
+        (swap! db assoc :entries entries)
+        (doall (for [entry entries]
+                 (go
+                   (let [id (:id entry)
+                         config-xml (<! (<ajax (str "/" id "/config.xml") :result "text"))
+                         config-dom (.parseFromString  (js/DOMParser.) config-xml "application/xml")
+                         config (dom->clj config-dom)
+                         widget (first (:children config))   
+                         config-elems (into {} (map (fn [e] [(conf-id e) e]) (:children widget)))
+                         ]
 
-                 (update-entry
-                   (into entry
-                         {:title (first (:children (:name config-elems)))   
-                          :date (first (:children (:date config-elems)))   
-                          :name (:id (:attrs widget))
-                          :version (:version (:attrs widget))
-                          :orientation (:value (:attrs (get config-elems "orientation"))) 
-                          :icon (str id "/"  (:src (:attrs (:icon config-elems))))
-                          :description (first (:children (:description config-elems)))  
-                          :shortdescription (first (:children (:shortdescription config-elems)))   })
-                   )
-                 ;(log entry title)
-                 )
-               )))
+                     (update-entry
+                      (into entry
+                            {:title (first (:children (:name config-elems)))   
+                             :date (first (:children (:date config-elems)))   
+                             :name (:id (:attrs widget))
+                             :version (:version (:attrs widget))
+                             :orientation (:value (:attrs (get config-elems "orientation"))) 
+                             :icon (str id "/"  (:src (:attrs (:icon config-elems))))
+                             :description (first (:children (:description config-elems)))  
+                             :shortdescription (first (:children (:shortdescription config-elems)))   })
+                      )
+                                        ;(log entry title)
+                     )
+                   )))
+        ))
+
+    ;; ## Routing
+    (aset js/window "onhashchange" update-route)
+    (update-route)
+    ;; ## /Initialisation
+    true
     ))
-
-;; ## Routing
-(aset js/window "onhashchange" update-route)
-(update-route)
-;; ## /Initialisation
-true
-))
 ;; # Styling
 (load-style! normalize-css "style-reset")
 (def entry-size 66)
@@ -147,176 +147,174 @@ true
 
 ;; ## Base style
 (load-style! 
-  {:body
-   {:background :white
-    :font-family :sans-serif}
+ {:body
+  {:background :white
+   :font-family :sans-serif}
 
-   :.info 
-   {:text-align :center
-    :margin "60px 0 60px 0" }
+  :.info 
+  {:text-align :center
+   :margin "60px 0 60px 0" }
 
-   "h1"
-   {:font-weight :normal}
+  "h1"
+  {:font-weight :normal}
 
-   :a 
-   {:text-decoration :none
-    :color "#00a"}
+  :a 
+  {:text-decoration :none
+   :color "#00a"}
 
-   ".date"
-   {:opacity "0.4"
-    :font-size "80%"
-    :font-weight :normal}
+  ".date"
+  {:opacity "0.4"
+   :font-size "80%"
+   :font-weight :normal}
 
-   :.nobr {:white-space :nowrap}
+  :.nobr {:white-space :nowrap}
 
-   :.clear
-   {:clear :both}
+  :.clear
+  {:clear :both}
 
-   :.solsort-entry
-   {:position :relative
-    :display :inline-block
-    :width entry-size
-    :height 120
-    :box-sizing :border-box
-    :margin-left 0
-    :margin-right box-margin
-    :vertical-align :top
-    :text-align :center
-    :font-size font-size
-    :font-weight weight
-    }
-   ".apps" 
-   {:display "inline-block"
-    :vertical-align :top
-    :box-sizing :border-box
-    :border-right "1px solid #eee"
-    :text-align :right
-    :width "38%" 
-    }
-   ".blog" 
-   {:display "inline-block"
-    :vertical-align :top
-    :box-sizing :border-box
-    :text-align :left
-    :width "61%" 
-    }
-   ".blog .post"
-   {:display "inline-block"
-    :text-align :left
-    :vertical-align :top
-    :font-size font-size
-    ;:font-size 12
-    :font-weight weight
-    :width (- 160 box-margin)
-    :margin-left box-margin
-    :padding-bottom (* 0.5 box-margin)
-    :overflow "hidden"
-    }
-   ".solsort-entry img"
-   {:width entry-size
-    :height entry-size
-    :border-radius box-margin
-    }
-}"basic-style"
-  )
+  :.solsort-entry
+  {:position :relative
+   :display :inline-block
+   :width entry-size
+   :height 120
+   :box-sizing :border-box
+   :margin-left 0
+   :margin-right box-margin
+   :vertical-align :top
+   :text-align :center
+   :font-size font-size
+   :font-weight weight
+   }
+  ".apps" 
+  {:display "inline-block"
+   :vertical-align :top
+   :box-sizing :border-box
+   :border-right "1px solid #eee"
+   :text-align :right
+   :width "38%" 
+   }
+  ".blog" 
+  {:display "inline-block"
+   :vertical-align :top
+   :box-sizing :border-box
+   :text-align :left
+   :width "61%" 
+   }
+  ".blog .post"
+  {:display "inline-block"
+   :text-align :left
+   :vertical-align :top
+   :font-size font-size
+                                        ;:font-size 12
+   :font-weight weight
+   :width (- 160 box-margin)
+   :margin-left box-margin
+   :padding-bottom (* 0.5 box-margin)
+   :overflow "hidden"
+   }
+  ".solsort-entry img"
+  {:width entry-size
+   :height entry-size
+   :border-radius box-margin}
+  } "basic-style")
 ;; ## Overlay
 (def device-ratio (/ 1080 1920))
 (load-style! 
-  { :.overlay
-   {:position :absolute
-    :top 0
-    :left 0
-    :width "100%"
-    :min-height "100%"
-    :text-align :center
-    }
-   ".overlay p"
-   {:text-align :left}
-   ".overlay .app"
-   {:display :inline-block
-    :padding 10
-    :margin-top 20
-    :width max-screen
-    :text-align :center
-    :box-shadow "4px 4px 12px rgba(0,0,0,0.5)"
-    :background "rgba(255,250,240,1)" 
-    
-    }
-   ".overlay .app .icon"
-   {:width 80
-    :height 80
-    :border-radius  (* 2 box-margin)
-    :margin-right 20
-    :vertical-align :top
-    :text-align :left }
-   :.title
-   {:text-align :left
-    :display "inline-block"}
-   ".overlay .app h1"
-   {:margin-top 0
-    :vertical-align :top}
+ { :.overlay
+  {:position :absolute
+   :top 0
+   :left 0
+   :width "100%"
+   :min-height "100%"
+   :text-align :center
+   }
+  ".overlay p"
+  {:text-align :left}
+  ".overlay .app"
+  {:display :inline-block
+   :padding 10
+   :margin-top 20
+   :width max-screen
+   :text-align :center
+   :box-shadow "4px 4px 12px rgba(0,0,0,0.5)"
+   :background "rgba(255,250,240,1)" 
+   
+   }
+  ".overlay .app .icon"
+  {:width 80
+   :height 80
+   :border-radius  (* 2 box-margin)
+   :margin-right 20
+   :vertical-align :top
+   :text-align :left }
+  :.title
+  {:text-align :left
+   :display "inline-block"}
+  ".overlay .app h1"
+  {:margin-top 0
+   :vertical-align :top}
 
-   "a.button" 
-   {:border "1px solid"
-    :display :inline-block
-    :border-radius 3
-    :padding "3px 5px 3px 5px"
-    :box-shadow "2px 2px 6px rgba(0,0,0,0.5)"
-    :margin "2px 8px 2px 0px"}
-   ;; ### demo-app
-   :.demo
-   {:background :white
-    :box-shadow "0px 0px 2px white"}
+  "a.button" 
+  {:border "1px solid"
+   :display :inline-block
+   :border-radius 3
+   :padding "3px 5px 3px 5px"
+   :box-shadow "2px 2px 6px rgba(0,0,0,0.5)"
+   :margin "2px 8px 2px 0px"}
+  ;; ### demo-app
+  :.demo
+  {:background :white
+   :box-shadow "0px 0px 2px white"}
 
-   :.portrait-app
-   {:height (+ 45 (* 0.5 app-size max-screen))
-    :text-align "center"}
-   :.landscape-app
-   {:height (+ 45 (* 0.5 app-size max-screen device-ratio))
-    :text-align "center"}
+  :.portrait-app
+  {:height (+ 45 (* 0.5 app-size max-screen))
+   :text-align "center"}
+  :.landscape-app
+  {:height (+ 45 (* 0.5 app-size max-screen device-ratio))
+   :text-align "center"}
 
-   ".portrait-app div" 
-   {:height (* 0.5 app-size max-screen)
-    :width (* 0.5 app-size max-screen device-ratio) }
-   ".landscape-app div"
-   {:width (* 0.5 app-size max-screen)
-    :height (* 0.5 app-size max-screen device-ratio) }
+  ".portrait-app div" 
+  {:height (* 0.5 app-size max-screen)
+   :width (* 0.5 app-size max-screen device-ratio) }
+  ".landscape-app div"
+  {:width (* 0.5 app-size max-screen)
+   :height (* 0.5 app-size max-screen device-ratio) }
 
-   :.device
-   {:display :inline-block
-    :background :black
-    :border-radius 10
-    :padding "15px 15px 30px 15px"
-    :box-shadow 
-    "inset 4px 4px 4px white,
+  :.device
+  {:display :inline-block
+   :background :black
+   :border-radius 10
+   :padding "15px 15px 30px 15px"
+   :box-shadow 
+   "inset 4px 4px 4px white,
      inset -2px -2px 2px white,
      3px 3px 12px rgba(0,0,0,0.5)"}
 
-   ".device iframe"
-   {:transform-origin "0 0"
-    :transform "scale(0.5)"
-    :border :none
-            
-            }
-   ".landscape-app iframe"
-   {:height (* app-size max-screen device-ratio)
-    :width (* app-size max-screen) }
-   ".portrait-app iframe"
-   {:width (* app-size max-screen device-ratio)
-    :height (* app-size max-screen) }
-   :.screenshot 
-   {:clear :both 
-    :max-height (* 0.5 max-screen)
-    :max-width (* 0.4 max-screen)
-    :margin-top 20
-    ;:box-shadow "4px 4px 12px rgba(0,0,0,0.5)"
-    } }, "overlay-style")
+  ".device iframe"
+  {:transform-origin "0 0"
+   :transform "scale(0.5)"
+   :border :none
+   
+   }
+  ".landscape-app iframe"
+  {:height (* app-size max-screen device-ratio)
+   :width (* app-size max-screen) }
+  ".portrait-app iframe"
+  {:width (* app-size max-screen device-ratio)
+   :height (* app-size max-screen) }
+  :.screenshot 
+  {:clear :both 
+   :max-height (* 0.5 max-screen)
+   :max-width (* 0.4 max-screen)
+   :margin-top 20
+                                        ;:box-shadow "4px 4px 12px rgba(0,0,0,0.5)"
+   } }, "overlay-style")
 ;; # View
 (defn render-date [date]
   (let [date (or date "    -00")]
-  [:div.date.nobr
-   (nth months (js/parseInt (.slice date 5 7) 10))
-   " " (.slice date 0 4) ])
+    [:div.date.nobr
+     (nth months (js/parseInt (.slice date 5 7) 10))
+     " " (.slice date 0 4) ])
 
   )
 (defn entry [o]
@@ -346,44 +344,44 @@ true
         url (str "http://apps.solsort.com/" id)
         ]
     (log "x" o)
-   [:div.overlay 
+    [:div.overlay 
      [:div.app
       [:a {:href url} [:img.icon {:src (str id "/icon.png")}]]
       [:div.title
-      (render-date (:date o))
-      [:h1 (:title o)]
-      [:p
-       [:a.button {:href url} "Try it"]
-       [:a.button {:href (str "https://github.com/" (:github o))} "Source code"]
-       
-       ]]
+       (render-date (:date o))
+       [:h1 (:title o)]
+       [:p
+        [:a.button {:href url} "Try it"]
+        [:a.button {:href (str "https://github.com/" (:github o))} "Source code"]
+        
+        ]]
       [:p (:shortdescription o)]
       [:div.clear]
       [:div {:class (if (= "portrait" (:orientation o)) "portrait-app" "landscape-app")}
-      [:div.device
+       [:div.device
         [:div.demo
          [:iframe {:src (str id "/index.html") :width "100%" :height "100%"}]]]]
       [:p (:description o)]
       [:hr]
-     [:a {:href url}
-      [:img.screenshot{:src (str id "/screenshot1.jpg") :on-error hide-elem}]
-     [:img.screenshot{:src (str id "/screenshot1.png") :on-error hide-elem}]
-     " \u00a0 "
-     [:img.screenshot{:src (str id "/screenshot2.png") :on-error hide-elem   }]
-     [:img.screenshot{:src (str id "/screenshot2.jpg") :on-error hide-elem   }]]
+      [:a {:href url}
+       [:img.screenshot{:src (str id "/screenshot1.jpg") :on-error hide-elem}]
+       [:img.screenshot{:src (str id "/screenshot1.png") :on-error hide-elem}]
+       " \u00a0 "
+       [:img.screenshot{:src (str id "/screenshot2.png") :on-error hide-elem   }]
+       [:img.screenshot{:src (str id "/screenshot2.jpg") :on-error hide-elem   }]]
       
       ]  
-   "overlay"])
+     "overlay"])
   )
 (defn content []
   (log @db)
   [:div 
-    {:on-click (set-route "")}
+   {:on-click (set-route "")}
    [:div.info
     [:h2 "Rasmus\u00a0Erik Voel\u00a0Jensen" ]
     [:h1 "solsort.com ApS"]
     [:div [:strong "HTML5 web/widgets/apps"]]
-    ;[:div "Writing code, creating things, solving problems"]
+                                        ;[:div "Writing code, creating things, solving problems"]
     [:div
      [:span "+45\u00a060703081"] " \u00a0 " 
      [:a {:href "mailto:hi@solsort.com?Subject=Hi"} "hi@solsort.com"] " "]
@@ -391,24 +389,24 @@ true
      [:a {:href "https://github.com/rasmuserik"} "GitHub"] " \u00A0 "
      [:a {:href "https://linkedin.com/in/rasmuserik"} "LinkedIn"] " "]
     [:div {:style {:max-width 300 :display :inline-block}}
-    [:h1 "NO PHONE "]     
-    "- my phone number is temporarily unavailable, catch me on email instead. It should hopefully be up and running in the middle of next week."]
-   ]
+     [:h1 "NO PHONE "]     
+     "- my phone number is temporarily unavailable, catch me on email instead. It should hopefully be up and running in the middle of next week."]
+    ]
    [:hr]
-   ; TODO: should be :entries,:posts subscription instead of db
+                                        ; TODO: should be :entries,:posts subscription instead of db
    [:div.apps
     (into [:div ] (map entry (:entries @db)))
     [:center
      [:a.button {:href "https://github.com/rasmuserik/writings/blob/master/sprints.md#current"} "Sprint Log"]
      #_[:a.button {:href "https://blog.solsort.com/rasmuserik"} "RasmusErik"]
      #_[:a.button {:href "https://blog.solsort.com/3-day-sprint"} "Product: 3-day Sprint"]
-    
-    ]
+     
+     ]
     
     ]
    [:div.blog
     (into [:div ] (map post (:posts @db))) ]
-    (if (second (:route @db)) [app-overlay] [:span])
+   (if (second (:route @db)) [app-overlay] [:span])
    ]
   )
 
